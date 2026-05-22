2c43059a4045b1105dfd0c8045cf18c33bd75101
WISTYY777 <a.molodjzhenya@e-u.edu.ua>
2026-05-22 16:01:53 +0300

    Update models.py

--- app/models/models.py
+++ app/models/models.py
@@ -1,6 +1,5 @@
-from datetime import datetime
-
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime
 
 from app.database import Base
 
@@ -9,15 +8,22 @@ from app.database import Base
 # У цьому файлі описується таблиця users через SQLAlchemy.
 # Модель User використовується для збереження логіна, email,
 # хешованого пароля, ПІБ, статусу активності та дати створення.
+# Ця структура потрібна для реєстрації, логіну та роботи JWT-автентифікації.
 
 
 class User(Base):
-    __tablename__ = "users"
+    tablename = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+
     username = Column(String, unique=True, index=True, nullable=False)
+
     email = Column(String, unique=True, index=True, nullable=False)
+
     password_hash = Column(String, nullable=False)
+
     is_active = Column(Boolean, default=True)
+
     full_name = Column(String, nullable=True)
+
     created_at = Column(DateTime, default=datetime.utcnow)
