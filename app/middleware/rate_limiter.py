6bbbed126458df49f9a4d34e603a616d6f2852af
WISTYY777 <a.molodjzhenya@e-u.edu.ua>
2026-05-23 15:41:46 +0300

    Update rate_limiter.py

--- app/middleware/rate_limiter.py
+++ app/middleware/rate_limiter.py
@@ -1,6 +1,8 @@
 # app/middleware/rate_limiter.py
+# ПР6: Артем — Rate Limiting, захист від Brute Force
 
 from slowapi import Limiter
 from slowapi.util import get_remote_address
 
+# Ідентифікує клієнта за IP-адресою
 limiter = Limiter(key_func=get_remote_address)
